4b1cafb82c7734451a5be6ade3cd65d0dc2ddc33
Louis-Saglio <louis.saglio@sfr.fr>
2018-02-03 02:20:02 +0100

    Use falcon http_error instead of http.HTTPStatus

--- system_expert/helpers.py
+++ system_expert/helpers.py
@@ -1,6 +1,2 @@
 def new_frozenset(*args) -> frozenset:
     return frozenset(args)
-
-
-def status(http_status):
-    return f"{http_status.value} {http_status.phrase}"
--- system_expert/restful_api.py
+++ system_expert/restful_api.py
@@ -1,12 +1,10 @@
 import json
-from http import HTTPStatus
 
 import falcon
-from falcon import Response, Request
+from falcon import Response, Request, HTTP_BAD_REQUEST, HTTP_CREATED, HTTP_INTERNAL_SERVER_ERROR
 
 from core import Engine, Fact
 from exceptions import BadFactField
-from helpers import status
 
 engine = Engine(set(), set())
 
@@ -16,26 +14,25 @@ class FactResource:
     @staticmethod
     def on_post(request: Request, response: Response):
         try:
-            # todo state is bool
             raw_json = request.stream.read()
             new_fact = Fact(**json.loads(raw_json, encoding='utf-8'))
             new_fact.check_fields()
             engine.facts.add(new_fact)
             response.media = {"message": "Fact sucessfully created", "fact_id": hash(new_fact)}
-            response.status = status(HTTPStatus.CREATED)
+            response.status = HTTP_CREATED
         except BadFactField as e:
             response.media = {"message": str(e)}
-            response.status = status(HTTPStatus.BAD_REQUEST)
+            response.status = HTTP_BAD_REQUEST
         except TypeError:
             response.media = {"message": "Bad post variable name"}
-            response.status = status(HTTPStatus.BAD_REQUEST)
+            response.status = HTTP_BAD_REQUEST
         except json.decoder.JSONDecodeError:
             response.media = {"message": f"Post data do not match JSON format. Received : {raw_json}"}
-            response.status = status(HTTPStatus.BAD_REQUEST)
+            response.status = HTTP_BAD_REQUEST
         except Exception as e:
             response.media = {"message": f"Unmanaged error : {e}. Please fix it here : "
                                          f"https://github.com/Louis-Saglio/PySystemExpert"}
-            response.status = status(HTTPStatus.INTERNAL_SERVER_ERROR)
+            response.status = HTTP_INTERNAL_SERVER_ERROR
             raise e
 
 
