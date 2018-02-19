99cc98eaf0403ff41019df58e0216776aeeb18c4
Louis-Saglio <louis.saglio@sfr.fr>
2018-02-19 22:46:38 +0100

    Servers address are now in config file

--- run.py
+++ run.py
@@ -6,13 +6,21 @@ from run.config import *
 
 
 chdir(SRC_ROOT_DIR)
-cmd = f"gunicorn -b :{WSGI_SERVER_PORT} {HTTP_APP_FILE.split('.')[0]}:{WSGI_API_VARIABLE_NAME}".split()
-gunicorn = Popen(cmd, stdout=DEVNULL, stderr=DEVNULL)
+gunicorn = Popen(
+    f"gunicorn -b {WSGI_SERVER_ADDRESS}:{WSGI_SERVER_PORT} {HTTP_APP_MODULE}:{WSGI_API_VARIABLE_NAME}".split(),
+    stdout=DEVNULL,
+    stderr=DEVNULL
+)
 chdir(PROJECT_ROOT_DIR)
 
 with open("Caddyfile", 'w') as f:
     with open(f"{RUN_ROOT_DIR}/Caddyfile.template", "r") as t:
-        f.write(t.read().replace('http_port', HTTP_SERVER_PORT).replace('wsgi_port', WSGI_SERVER_PORT))
+        f.write(t.read()
+                .replace('http_port', HTTP_SERVER_PORT)
+                .replace('wsgi_port', WSGI_SERVER_PORT)
+                .replace('http_addr', HTTP_SERVER_ADDRESS)
+                .replace('wsgi_addr', WSGI_SERVER_ADDRESS)
+                )
 
 
 caddy = Popen(["caddy"], stdout=DEVNULL, stderr=DEVNULL)
--- run/config.py
+++ run/config.py
@@ -4,8 +4,11 @@ PROJECT_ROOT_DIR = getcwd()
 SRC_ROOT_DIR = "system_expert"
 RUN_ROOT_DIR = "run"
 
-HTTP_APP_FILE = "restful_api.py"
+HTTP_APP_MODULE = "restful_api.py"
 WSGI_API_VARIABLE_NAME = "api"
 
 WSGI_SERVER_PORT = "8888"
 HTTP_SERVER_PORT = "8000"
+
+WSGI_SERVER_ADDRESS = "127.0.0.1"
+HTTP_SERVER_ADDRESS = "127.0.0.1"
