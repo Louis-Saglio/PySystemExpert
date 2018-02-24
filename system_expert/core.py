8bcb4ee357c87b7c4e813a148f0c36105b5a2600
Louis-Saglio <louis.saglio@sfr.fr>
2018-02-24 21:57:21 +0100

    Add DataManager and SystemExpert with skeleton prototype methods

--- system_expert/core.py
+++ system_expert/core.py
@@ -1,6 +1,11 @@
 import copy
+import os
+import random
+import sqlite3
+import string
 from typing import Set, FrozenSet, Any
 
+import config
 from exceptions import BadFactField
 
 
@@ -98,3 +103,55 @@ class Engine:
                 break
             else:
                 assert len(self.facts) > nbr_facts
+
+
+class DataManager:
+
+    def __init__(self):
+        self.connexion = sqlite3.connect(os.path.join(config.PROJECT_ROOT_DIR, config.DATA_BASE_FILE))
+
+    def _get_user(self, uuid: str):
+        return self.connexion.cursor().execute(
+            "SELECT id FROM users WHERE uuid = ?",
+            (uuid,)
+        ).fetchone()[0]
+
+    def add_fact(self, uuid: str, fact: Fact):
+        cursor = self.connexion.cursor()
+        cursor.execute(
+            "INSERT INTO facts (name, value, state, type, user_id) VALUES (?,?,?,?,?)",
+            (fact.NAME, fact.VALUE, fact.STATE, str(fact.VALUE.__class__), self._get_user(uuid))
+        )
+        self.connexion.commit()
+
+    def __del__(self):
+        self.connexion.close()
+
+    def create_user(self):
+        cursor = self.connexion.cursor()
+        uuid = ''.join(random.sample(string.ascii_letters + string.digits, config.USER_UUID_LENGTH))
+        cursor.execute("INSERT INTO users (uuid) VALUES (?)", (uuid,))
+        self.connexion.commit()
+        return uuid
+
+
+class SystemExpert:
+
+    def __init__(self):
+        self.engine = Engine(set(), set())
+        self.data_manager = DataManager()
+
+    def get_api_key(self):
+        return self.data_manager.create_user()
+
+    def add_fact(self, uuid: str, fact: Fact):
+        self.data_manager.add_fact(uuid, fact)
+
+    def add_rule(self, uuid: str, rule: Rule):
+        pass
+
+    def process(self, uuid: str):
+        pass
+
+    def get_facts(self, uuid: str) -> Set[Fact]:
+        pass
