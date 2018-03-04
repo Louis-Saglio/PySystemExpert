2fc3b62ab447d9906c5ee2b52ec9ca5c8bbefc66
Louis-Saglio <louis.saglio@sfr.fr>
2018-03-05 00:19:05 +0100

    [refactor] data_lib reorganization ended

--- system_expert/data_lib/data_manager.py
+++ system_expert/data_lib/data_manager.py
@@ -1,4 +1,18 @@
+import sqlite3
+from typing import Hashable
+
+import os
+
+CREATE_DB_SCRIPT = os.path.join(os.path.dirname(__file__), 'create_db.sql')
+
+
 class DataManager:
 
-    def add_user(self):
+    def __init__(self, db_path):
+        self.connexion = sqlite3.connect(db_path)
+
+    def create_instance(self) -> str:
+        pass
+
+    def add_fact(self, instance_id: str, name: str, value: Hashable, state: bool):
         pass
---
+++ system_expert/data_lib/tests.py
@@ -0,0 +1,38 @@
+import unittest
+
+from data_manager import DataManager, CREATE_DB_SCRIPT
+
+
+class TestDataManager(unittest.TestCase):
+
+    def setUp(self):
+        self.data_manager = DataManager(':memory:')
+        with open(CREATE_DB_SCRIPT) as f:
+            self.data_manager.connexion.executescript(f.read())
+        self.data_manager.connexion.execute("INSERT INTO users (uuid) VALUES ('test_user')")
+        self.data_manager.connexion.commit()
+
+    def test_add_user(self):
+        uuid = self.data_manager.create_instance()
+        self.assertEqual(
+            uuid,
+            self.data_manager.connexion.execute("SELECT uuid FROM users WHERE id = 2").fetchone()[0]
+        )
+
+    # def test__get_user(self):
+    #     self.data_manager.connexion.execute(
+    #         "INSERT INTO users (uuid) VALUES ('foobar_uuid')"
+    #     )
+    #     self.assertEqual(
+    #         2,
+    #         self.data_manager._get_user("foobar_uuid")
+    #     )
+
+    def test_add_fact(self):
+        self.data_manager.add_fact("test_user", "fact", "val", True)
+        self.assertEqual(
+            self.data_manager.connexion.execute(
+                "SELECT name, value, state, type, uuid FROM facts f JOIN users u ON f.user_id = u.id"
+            ).fetchone(),
+            ('fact', 'val', 1, 'str', 'test_user')
+        )
