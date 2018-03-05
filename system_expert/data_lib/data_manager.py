b83907093d81c016df881b01dc10ab3474894c76
Louis-Saglio <louis.saglio@sfr.fr>
2018-03-05 21:54:19 +0100

    [new] Implement add DataManage.fact

--- system_expert/data_lib/data_manager.py
+++ system_expert/data_lib/data_manager.py
@@ -7,11 +7,11 @@ CREATE_DB_SCRIPT = os.path.join(os.path.dirname(__file__), 'create_db.sql')
 
 class DataManager:
 
-    def __init__(self, db_path):
+    def __init__(self, db_path: str):
         self.connexion = sqlite3.connect(db_path)
         self.db_path = db_path
 
-    def create_db(self,):
+    def create_db(self):
         self.connexion.executescript(CREATE_DB_SCRIPT)
 
     def create_instance(self, uuid: str):
@@ -19,6 +19,17 @@ class DataManager:
             "INSERT INTO users (uuid) VALUES (?)",
             (uuid,)
         )
+        self.connexion.commit()
+
+    def _get_user_id(self, uuid: str) -> int:
+        return self.connexion.execute(
+            "SELECT id FROM users WHERE uuid = ?",
+            (uuid,)
+        ).fetchone()[0]
 
     def add_fact(self, instance_id: str, name: str, value: Hashable, state: bool):
-        pass
+        self.connexion.execute(
+            "INSERT INTO facts (name, value, state, type, user_id) VALUES (?,?,?,?,?)",
+            (name, value, state, type(value).__name__, instance_id)
+        )
+        self.connexion.commit()
--- system_expert/data_lib/tests.py
+++ system_expert/data_lib/tests.py
@@ -20,20 +20,21 @@ class TestDataManager(unittest.TestCase):
             self.data_manager.connexion.execute("SELECT uuid FROM users WHERE id = 2").fetchone()[0]
         )
 
-    # def test__get_user(self):
-    #     self.data_manager.connexion.execute(
-    #         "INSERT INTO users (uuid) VALUES ('foobar_uuid')"
-    #     )
-    #     self.assertEqual(
-    #         2,
-    #         self.data_manager._get_user("foobar_uuid")
-    #     )
+    def test__get_user_id(self):
+        self.data_manager.connexion.execute(
+            "INSERT INTO users (uuid) VALUES ('foobar_uuid')"
+        )
+        self.assertEqual(
+            2,
+            self.data_manager._get_user_id("foobar_uuid")
+        )
 
     def test_add_fact(self):
         self.data_manager.add_fact("test_user", "fact", "val", True)
         self.assertEqual(
+            ('fact', 'val', 1, 'str', 'test_user'),
             self.data_manager.connexion.execute(
-                "SELECT name, value, state, type, uuid FROM facts f JOIN users u ON f.user_id = u.id"
-            ).fetchone(),
-            ('fact', 'val', 1, 'str', 'test_user')
+                "SELECT name, value, state, type, uuid FROM facts f JOIN users u ON f.user_id = ?",
+                ("test_user",)
+            ).fetchone()
         )
