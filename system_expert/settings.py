ef34eb63a84c438a885df2ace841e33ad6356280
Louis-Saglio <louis.saglio@sfr.fr>
2018-03-05 23:44:02 +0100

    [bugfix] system_expert.settings.DATA_BASE_FILE return file path, not its parent directory

--- system_expert/settings.py
+++ system_expert/settings.py
@@ -2,7 +2,9 @@ import string
 
 import os
 
-DATA_BASE_FILE = os.path.join(os.path.dirname(__file__))
+DATA_BASE_FILE = os.path.join(os.path.dirname(__file__), "database.sqlite3")
 
 USER_UUID_LENGTH = 16
 USER_UUID_CHARS_POOL = string.ascii_letters + string.digits
+
+DEBUG = True
