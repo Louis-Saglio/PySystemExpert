eece353f7b5f89ca94f5f141cb590db662101a2e
Louis-Saglio <louis.saglio@sfr.fr>
2018-03-10 12:22:24 +0100

    [refactor] Factorize user creation in SystemExpert tests [refactor] Remove DataManager feature tests in SystemExpert tests when DataManager implementation advancement allow it

--- system_expert/tests.py
+++ system_expert/tests.py
@@ -8,6 +8,7 @@ class TestSystemExpert(unittest.TestCase):
 
     def setUp(self):
         self.system_expert = core.SystemExpert()
+        self.user_uuid = self.system_expert.create_user()
 
     def test__get_user_id(self):
         settings.USER_UUID_LENGTH = 3
@@ -25,36 +26,27 @@ class TestSystemExpert(unittest.TestCase):
         self.assertRaises(UserWarning, self.system_expert._get_new_user_id)
 
     def test_add_fact(self):
-        user_uuid = self.system_expert.create_user()
-        fact_id = self.system_expert.add_fact(user_uuid, 'fact', 42, False)
-        fact_data = self.system_expert.data_manager.connexion.execute(
-            "SELECT name, value, state, type, user_id FROM facts WHERE id = ?",
-            (fact_id,)
-        ).fetchone()
-        self.assertEqual(
-            ('fact', '42', 0, 'int', self.system_expert.data_manager._get_user_id(user_uuid)),
-            fact_data
-        )
+        fact_id = self.system_expert.add_fact(self.user_uuid, 'fact', 42, False)
+        fact_data = self.system_expert.data_manager.get_fact(self.user_uuid, fact_id)
+        self.assertEqual(('fact', 42, 0), fact_data)
 
     def test_get_fact(self):
-        # Assumes that add_fact and create_user works
-        user_uuid = self.system_expert.create_user()
-        fact_id = self.system_expert.add_fact(user_uuid, "fact", 42, True)
+        # Assumes that add_fact works
+        fact_id = self.system_expert.add_fact(self.user_uuid, "fact", 42, True)
         self.assertEqual(
             ("fact", 42, True),
-            self.system_expert.get_fact(user_uuid, fact_id)
+            self.system_expert.get_fact(self.user_uuid, fact_id)
         )
 
     def test_add_rule(self):
-        # Assumes that add_fact and create_user works
-        user_uuid = self.system_expert.create_user()
+        # Assumes that add_fact
         majors = (
-            self.system_expert.add_fact(user_uuid, "fact1", 15, True),
-            self.system_expert.add_fact(user_uuid, "fact2", b"val", False)
+            self.system_expert.add_fact(self.user_uuid, "fact1", 15, True),
+            self.system_expert.add_fact(self.user_uuid, "fact2", b"val", False)
         )
-        conclusions = (self.system_expert.add_fact(user_uuid, "fact2", 0.77, True),)
+        conclusions = (self.system_expert.add_fact(self.user_uuid, "fact2", 0.77, True),)
 
-        rule_id = self.system_expert.add_rule(user_uuid, majors, conclusions)
+        rule_id = self.system_expert.add_rule(self.user_uuid, majors, conclusions)
 
         expected_rule_id = self.system_expert.data_manager.connexion.execute(
             "SELECT id FROM rules WHERE id = ?",
@@ -75,11 +67,9 @@ class TestSystemExpert(unittest.TestCase):
 
     def test_get_facts(self):
         # Assumes that add_fact works
-        # Assumes that DataManager.get_facts works
-        user_uuid = self.system_expert.create_user()
-        self.system_expert.add_fact(user_uuid, 'name1', 42, False)
-        self.system_expert.add_fact(user_uuid, 'name2', 0.42, True)
+        self.system_expert.add_fact(self.user_uuid, 'name1', 42, False)
+        self.system_expert.add_fact(self.user_uuid, 'name2', 0.42, True)
         self.assertEqual(
-            self.system_expert.data_manager.get_facts(user_uuid),
-            self.system_expert.get_facts(user_uuid)
+            self.system_expert.data_manager.get_facts(self.user_uuid),
+            self.system_expert.get_facts(self.user_uuid)
         )
